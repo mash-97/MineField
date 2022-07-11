1841b8d85cc67a2fee2a8d3ac4e470424c262533
mash-97 <mahimnhd97@gmail.com>
2022-07-12 02:22:05 +0600

    unusuable test on area_test.rb

---
+++ area_test.rb
@@ -0,0 +1,21 @@
+def square_area(x,y)
+  return x*1.0*y
+end
+
+def triangle_area(x,y)
+  return (x*1.0*y)/2
+end
+
+
+
+def __test__()
+  y = 2
+  60.times.collect{(12..3423).to_a.sample}.each do |x|
+    puts("y: #{y}, x: #{x}, ssa: #{square_area(x,y).round(4)}, ta: #{triangle_area(x,y).round(4)}")
+  end
+end
+
+if $0 == __FILE__ then
+  __test__()
+end
+
