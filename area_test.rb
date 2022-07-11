d4d587c64f64df4044458d7146032ba6245220b2
mash-97 <mahimnhd97@gmail.com>
2022-07-12 02:37:53 +0600

    unusable test

--- area_test.rb
+++ area_test.rb
@@ -11,7 +11,10 @@ end
 def __test__()
   y = 2
   60.times.collect{(12..3423).to_a.sample}.each do |x|
-    puts("y: #{y}, x: #{x}, ssa: #{square_area(x,y).round(4)}, ta: #{triangle_area(x,y).round(4)}")
+    st = nil
+    # st = x.times.collect{|i| (y*1.0*x).round(4)}.sum
+    # st = 
+    puts("y: #{y}, x: #{x}, ssa: #{square_area(x,y).round(4)}, ta: #{triangle_area(x,y).round(4)}, st: #{st}")
   end
 end
 
